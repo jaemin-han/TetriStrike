06494c7b56405ddb66d390840fec432ff1a9f50b
eoneee <sangeon0412@gmail.com>
2024-09-05 19:17:17 +0900

    [fix] fix...

--- Source/TetriStrike/TetriStrikeCharacter.cpp
+++ Source/TetriStrike/TetriStrikeCharacter.cpp
@@ -63,22 +63,7 @@ void ATetriStrikeCharacter::BeginPlay()
 		
 	}
 
-	APlayerController* PlayerController = Cast<APlayerController>(GetController());
-	if(MainUIFactory && PlayerController)
-	{
-		UMainWidget* MainUI = CreateWidget<UMainWidget>(PlayerController,MainUIFactory);
-		if(MainUI)
-		{
-			MainUI->AddToViewport();
-			MainUI->WeaponComponent = TP_WeaponComponent;
-
-			if(TP_WeaponComponent)
-			{
-				TP_WeaponComponent->SetMainUI(MainUI);
-			}
-			//MainUI->UpdateRadialSlider();
-		}
-	}
+	
 
 }
 
